1d0b13ef8157ce87463c5b51893f0dfc53d4af55
Denis <Denis120179@yandex.by>
2023-06-25 12:27:57 +0300

    333

--- dz_4.java
+++ dz_4.java
@@ -1,38 +1,26 @@
-// Программа для реализации операции вставки в красно-черном дереве.
 import java.util.Scanner;
-
 class node {
-
   node left, right;
   int data;
-
   // красный ==> true, черный ==> false
   boolean color;
-
   node(int data) {
     this.data = data;
     left = null;
     right = null;
-
 // Новый узел, который создается, является всегда красного цвета.
     color = true;
   }
 }
-
 public class dz_4 {
-
 public static void main(String[] args) {
-
     dz_4 node = new dz_4();
     Scanner scan = new Scanner(System.in);
-
     char ch;
     do {
       System.out.println("Введите целое число");
-
       int num = scan.nextInt();
       root = node.insert(root, num);
-
       node.inorder(root);
       System.out.println("\nВы хотите продолжить? (введите y или n)");
       ch = scan.next().charAt(0);
@@ -41,31 +29,27 @@ public static void main(String[] args) {
 
   private static node root = null;
 
-  // Функция для поворота узла против часовой стрелки.
+  // поворот узла против часовой стрелки.
   node rotateLeft(node myNode) {
     System.out.printf("поворот влево!!\n");
     node child = myNode.right;
     node childLeft = child.left;
-
     child.left = myNode;
     myNode.right = childLeft;
-
     return child;
   }
 
-  // Функция для поворота узла по часовой стрелке.
+  // поворот узла по часовой стрелке.
   node rotateRight(node myNode) {
     System.out.printf("вращение вправо\n");
     node child = myNode.left;
     node childRight = child.right;
-
     child.right = myNode;
     myNode.left = childRight;
-
     return child;
   }
 
-  // Функция для проверки того, является ли узел красного цвета или нет.
+  // проверка того, является ли узел красного цвета
   boolean isRed(node myNode) {
     if (myNode == null) {
       return false;
@@ -73,20 +57,19 @@ public static void main(String[] args) {
     return (myNode.color == true);
   }
 
-  // Функция для изменения цвета двух узлы.
+  // изменение цвета 
   void swapColors(node node1, node node2) {
     boolean temp = node1.color;
     node1.color = node2.color;
     node2.color = temp;
   }
 
-  // вставка в левостороннее Красно-черное дерево.
+  // вставка в левостороннее красно-черное дерево.
   node insert(node myNode, int data) {
-// Обычный код вставки для любого двоичного файла
+// вставка для любого двоичного файла
     if (myNode == null) {
       return new node(data);
     }
-
     if (data < myNode.data) {
       myNode.left = insert(myNode.left, data);
     } else if (data > myNode.data) {
@@ -95,35 +78,21 @@ public static void main(String[] args) {
       return myNode;
     }
 
-// случай 1.
-    // когда правый дочерний элемент красный, а левый дочерний элемент черный или не существует.
+    
     if (isRed(myNode.right) && !isRed(myNode.left)) {
-// Повернуть узел  влево
-      myNode = rotateLeft(myNode);
-
-// Поменять местами цвета дочернего узла всегда должен быть красным
-      swapColors(myNode, myNode.left);
+    myNode = rotateLeft(myNode);
+    swapColors(myNode, myNode.left);    
     }
-
-// случай 2
-    // когда левый ребенок, а также левый внук выделены красным цветом
     if (isRed(myNode.left) && isRed(myNode.left.left)) {
-// Повернуть узел в право
       myNode = rotateRight(myNode);
       swapColors(myNode, myNode.right);
     }
 
-// случай 3
-    // когда и левый, и правый дочерние элементы окрашены в красный цвет.
     if (isRed(myNode.left) && isRed(myNode.right)) {
-// Инвертировать цвет узла это левый и правый дети.
-      myNode.color = !myNode.color;
-
-// Изменить цвет на черный.
-      myNode.left.color = false;
-      myNode.right.color = false;
+    myNode.color = !myNode.color;
+    myNode.left.color = false;
+    myNode.right.color = false;
     }
-
     return myNode;
   }
 
